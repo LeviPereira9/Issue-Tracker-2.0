a65505c181d584eb4c5beebcded10892907fdf61
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2023-03-23 20:10:16 -0300

    Navbar - Render Auth

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,92 +1,195 @@
+//Router-Dom
+import { NavLink } from 'react-router-dom';
+
+//Bootstrap
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink } from 'react-router-dom';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+//Icons
+import { MdAdminPanelSettings } from 'react-icons/md';
+import { BsPersonCircle } from 'react-icons/bs';
 
 type Props = {};
 
-const NavBar = (props: Props) => {
-  return (
-    <Navbar collapseOnSelect expand="lg" className="navbar">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/" className="navbar__brand">
-          Track Issue 2.0
-        </Navbar.Brand>
-        <div className='d-flex'>
-        <Nav className='d-flex d-lg-none flex-row '>
-            <Nav.Link as={NavLink} to="/register" className='navbar__link me-2'>
-              Sign Up
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/login" className='navbar__link px-3 me-3 navbar-button'>
-              Sign In
-            </Nav.Link>
-          </Nav>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        </div>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/" className="navbar__link">
-              Home
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/priorities" className="navbar__link">
-              Priorities
-            </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/secure_practices"
-              className="navbar__link"
-            >
-              Secure Practices
-            </Nav.Link>
-            <NavDropdown
-              title="Tickets"
-              id="collasible-nav-dropdown"
-              className="navbar__link"
-            >
-              <NavDropdown.Item
-                as={NavLink}
-                to="/anonymous_ticket"
-                className="navbar__link"
-              >
-                Open anonymous Ticket
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                as={NavLink}
-                to="/internal_ticket"
-                className="navbar__link"
-              >
-                Open internal Ticket
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                as={NavLink}
-                to="/tickets"
-                className="navbar__link"
-              >
-                My Tickets
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={NavLink} to="/faq" className="navbar__link">
-              FAQ
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-      <Nav className="d-none d-lg-flex">
-        <Nav.Link as={NavLink} to="/register" className="navbar__link me-2">
+const NAVBAR_CLASS = {
+  link: 'navbar__link ',
+  icon: 'navbar__icon ',
+  button: 'navbar__button ',
+  userDropdown: 'navbar__userDropdown ',
+};
+
+function renderAdminAuth() {
+  if (true) {
+    return (
+      <>
+        <MdAdminPanelSettings
+          className={`${NAVBAR_CLASS.icon} me-4`}
+          size={'2.7em'}
+        />
+      </>
+    );
+  }
+}
+
+function renderUserAuth() {
+  if (!false) {
+    return (
+      <>
+        {renderAdminAuth()}
+        <NavDropdown
+          active={true}
+          title={
+            <>
+              <BsPersonCircle className={`${NAVBAR_CLASS.icon}`} size={'2em'} />
+            </>
+          }
+          id="collasible-nav-dropdown"
+          className={`${
+            NAVBAR_CLASS.link + NAVBAR_CLASS.userDropdown
+          } me-4`}
+        >
+          <NavDropdown.Item
+            as={NavLink}
+            to="/perfil"
+            className={`${NAVBAR_CLASS.link}`}
+          >
+            Profile
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            as={NavLink}
+            to="/settings"
+            className={`${NAVBAR_CLASS.link}`}
+          >
+            Settings
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            as={NavLink}
+            to="/help"
+            className={`${NAVBAR_CLASS.link}`}
+          >
+            Help
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item className={`${NAVBAR_CLASS.link}`}>
+            Sign Out
+          </NavDropdown.Item>
+        </NavDropdown>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Nav.Link
+          as={NavLink}
+          to="/register"
+          className={`${NAVBAR_CLASS.link} me-2`}
+        >
           Sign Up
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/login"
-          className="navbar__link px-3 me-3 navbar-button"
+          className={`${NAVBAR_CLASS.button} px-3 me-3`}
         >
           Sign In
         </Nav.Link>
-      </Nav>
-    </Navbar>
+      </>
+    );
+  }
+}
+
+const NavBar = (props: Props) => {
+  return (
+    <>
+      <Navbar bg="light" expand="md" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand as={NavLink} to="/">
+            Track Issue 2.0
+          </Navbar.Brand>
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-md"
+            aria-labelledby="offcanvasNavbarLabel-expand-md"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel-expand-md">
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-start flex-grow-1 pe-3">
+                <Nav.Link
+                  as={NavLink}
+                  to="/"
+                  className={`${NAVBAR_CLASS.link}`}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/priorities"
+                  className={`${NAVBAR_CLASS.link}`}
+                >
+                  Priorities
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/secure_practices"
+                  className={`${NAVBAR_CLASS.link}`}
+                >
+                  Secure Practices
+                </Nav.Link>
+                <NavDropdown
+                  title="Tickets"
+                  id="collasible-nav-dropdown"
+                  className={`${NAVBAR_CLASS.link}`}
+                >
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/anonymous_ticket"
+                    className={`${NAVBAR_CLASS.link}`}
+                  >
+                    Anonymous
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/internal_ticket"
+                    className={`${NAVBAR_CLASS.link}`}
+                  >
+                    Internal
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/tickets"
+                    className={`${NAVBAR_CLASS.link}`}
+                  >
+                    My Tickets
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link
+                  as={NavLink}
+                  to="/faq"
+                  className={`${NAVBAR_CLASS.link}`}
+                >
+                  FAQ
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+          <Nav className="flex-row align-items-center">
+            {renderUserAuth()}
+            <Navbar.Toggle aria-controls="offcanvasNavbar-expand-md" />
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
+
+// eslint-disable-next-line no-lone-blocks
 
 export default NavBar;
