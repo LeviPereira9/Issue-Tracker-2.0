1ed84095c3cca2ebc21867bb25b615e0078054db
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2023-04-04 19:39:37 -0300

    Priorities - Comment Section

--- src/pages/Priorities.tsx
+++ src/pages/Priorities.tsx
@@ -3,9 +3,12 @@ import { Container } from 'react-bootstrap';
 import PrioritiesCards from '../components/layout/priorities/PrioritiesCards';
 
 //Icons
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { TbClockHour2 } from 'react-icons/tb';
+import { BsShieldFillExclamation, BsShieldFillCheck, BsThreeDots } from 'react-icons/bs';
 
 const Priorities = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const issueExemple = {
     docId: 'Canvote',
     creatorId: 'b35iITFkUxManITs2evFfdJywFr1',
@@ -51,7 +54,7 @@ const Priorities = () => {
       closedAt: undefined,
       expectedConclusion: '24 hours',
       status: {
-        mode: 2,
+        mode: 1,
         text: 'In progress',
       },
       priority: 'high',
@@ -67,10 +70,16 @@ const Priorities = () => {
         last: '31/03/2023 - 16:24:59',
         comments: [
           {
+            id: 'abada',
+            img: 'https://via.placeholder.com/75',
+            name: 'Augustinho Carrara',
             at: '31/03/2023 - 16:13:22',
             text: 'The team has received the call and is currently prioritizing it. We have checked the other social media accounts and despite attempts to access them, no other accounts have been violated.',
           },
           {
+            id: 'Mangão',
+            img: 'https://via.placeholder.com/75',
+            name: 'Michael Jackson da Silva',
             at: '31/03/2023 - 16:24:59',
             text: 'An announcement has been made on social media alerting that we are not in control of the Instagram account and advising not to continue any conversation at the moment.',
           },
@@ -95,89 +104,139 @@ const Priorities = () => {
             <section key={issue.docId} className="priorities-issue-panel">
               <div className="priorities-issue-panel-header">
                 <h3 className="priorities-issue-panel-header__title">
-                  <TbClockHour2 /> {issue.docId}
+                  {issue.status.mode !== 2 ? (
+                    <span className="priorities-issue-panel-header__title-icon processing">
+                      <BsShieldFillExclamation />
+                    </span>
+                  ) : (
+                    <span className="priorities-issue-panel-header__title-icon checked">
+                      <BsShieldFillCheck />
+                    </span>
+                  )}{' '}
+                  {issue.docId}
                 </h3>
               </div>
               <div className="priorities-issue-panel-infos row">
-                <div className="priorities-issue-panel-infos-left col-lg-6">
+                <div className="priorities-issue-panel-infos-left col-md-6">
                   <table>
                     <thead>
                       <tr>
-                        <th colSpan={2}>
+                        <th className="table-title" colSpan={2}>
                           Issue Details
                         </th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td className='table-title'>ID:</td>
-                        <td className='table-value'>{issue.docId}</td>
+                        <td className="table-key">ID:</td>
+                        <td className="table-value">{issue.docId}</td>
                       </tr>
                       <tr>
-                        <td className='table-title'>Creator:</td>
-                        <td className='table-value'>{issue.creator.name}</td>
+                        <td className="table-key">Creator:</td>
+                        <td className="table-value">{issue.creator.name}</td>
                       </tr>
                       <tr>
-                        <td className='table-title'>Department:</td>
-                        <td className='table-value'>{issue.creator.department}</td>
+                        <td className="table-key">Department:</td>
+                        <td className="table-value">
+                          {issue.creator.department}
+                        </td>
                       </tr>
                       <tr>
-                        <td className='table-title'>Created At:</td>
-                        <td className='table-value'>{issue.createdAt}</td>
+                        <td className="table-key">Created At:</td>
+                        <td className="table-value">{issue.createdAt}</td>
                       </tr>
                       <tr>
-                        <td className='table-title'>{issue.closedAt ? 'Closed At: ' : 'Due Date: '}</td>
-                        <td className='table-value'>
+                        <td className="table-key">
+                          {issue.closedAt ? 'Closed At: ' : 'Due Date: '}
+                        </td>
+                        <td className="table-value">
                           {issue.closedAt
                             ? issue.closedAt
                             : issue.expectedConclusion}
                         </td>
                       </tr>
                       <tr>
-                        <td className='table-title'>Subject:</td>
-                        <td className='table-value'>{issue.subject}</td>
+                        <td className="table-key">Subject:</td>
+                        <td className="table-value">{issue.subject}</td>
                       </tr>
                       <tr>
-                        <td className='table-title'>Description:</td>
-                        <td className='table-value'>{issue.desc}</td>
+                        <td className="table-key">Description:</td>
+                        <td className="table-value">{issue.desc}</td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
-                <div className="priorities-issue-panel-infos-right col-lg-6">
+                <div className="priorities-issue-panel-infos-right col-md-6">
                   <table>
                     <thead>
                       <tr>
-                        <th colSpan={2}>Dealings Details</th>
+                        <th className="table-title" colSpan={2}>
+                          Dealings Details
+                        </th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td className='table-title'>Status:</td>
-                        <td className='table-value'>{issue.status.text}</td>
+                        <td className="table-key">Status:</td>
+                        <td className="table-value">{issue.status.text}</td>
                       </tr>
                       <tr>
-                        <td className='table-title'>Priority:</td>
-                        <td className='table-value'>{issue.priority}</td>
+                        <td className="table-key">Priority:</td>
+                        <td className="table-value">{issue.priority}</td>
                       </tr>
                       <tr>
-                        <th className='pt-2 pb-1' colSpan={2}>Impact Details</th>
+                        <th className="pt-2 pb-1" colSpan={2}>
+                          Impact Details
+                        </th>
                       </tr>
                       <tr>
-                        <td className='table-title'>Level:</td>
-                        <td className='table-value'>{issue.impact.level.text}</td>
+                        <td className="table-key">Level:</td>
+                        <td className="table-value">
+                          {issue.impact.level.text}
+                        </td>
                       </tr>
                       <tr>
-                        <td className='table-title'>on Clients:</td>
-                        <td className='table-value'>{issue.impact.onClients ? 'Yes' : 'No'}</td>
+                        <td className="table-key">on Clients:</td>
+                        <td className="table-value">
+                          {issue.impact.onClients ? 'Yes' : 'No'}
+                        </td>
                       </tr>
                       <tr>
-                        <td className='table-title'>Departments:</td>
-                        <td className='table-value'>{issue.impact.deparments}</td>
+                        <td className="table-key">Departments:</td>
+                        <td className="table-value">
+                          {issue.impact.deparments.join(', ') + '.'}
+                        </td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
+              </div>
+              <div className="priorities-issue-panel-comments">
+                {issue.update.comments.map(comment => (
+                  <div key={comment.id} className="comment">
+                    <div className="comment-userProfile">
+                      <img
+                        src={comment.img}
+                        alt="User profile"
+                        className="comment-userProfile-img"
+                      />
+                    </div>
+                    <div className="comment-info">
+                      <div className="comment-info-header">
+                        <div className="comment-info-header-user">
+                          <span className="h6">
+                            {comment.name} •
+                          </span>
+                          <span className="comment-info__at"> {comment.at}</span>
+                        </div>
+                        <div className="comment-info-header-tools">
+                          <BsThreeDots/>
+                        </div>
+                      </div>
+                      <p className="comment-info__text">{comment.text}</p>
+                    </div>
+                  </div>
+                ))}
               </div>
             </section>
           ))}
